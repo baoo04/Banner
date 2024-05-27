48e2cff87ecda2a0b90c7bb0fd614f4901e414f9
Sotatek-LamHo <kent.nguyen@silicondunes.ae>
2024-05-27 22:05:15 +0700

    update config

--- src/app/ui-kits/page.tsx
+++ src/app/ui-kits/page.tsx
@@ -7,6 +7,7 @@ import Button from "@/components/ui/Button";
 import Dropdown from "@/components/ui/Dropdown";
 import { useState } from "react";
 import Pagination from "@/components/ui/Pagination";
+import Image from "next/image";
 
 export default function UIKits() {
   const [selectedItem, setSelectedItem] = useState("");
@@ -39,6 +40,14 @@ export default function UIKits() {
             <CartPlus /> Disabled Primary Button
           </Button>
         </div>
+
+        <Image
+          priority={false}
+          alt=""
+          width={1000} // a fixed value for max-width
+          height={500} // whatever
+          src="https://images.unsplash.com/photo-1653832919710-348081cb9e87?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
 
         <h1>Inputs</h1>
         <Input
